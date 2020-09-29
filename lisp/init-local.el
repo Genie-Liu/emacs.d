7685920ca6157f1992c30a5c4abd23289c1b078b
liujianshi <liujianshi@meituan.com>
2020-09-29 11:20:56 +0800

    add personal settings

---
+++ lisp/init-local.el
@@ -0,0 +1,8 @@
+;;; init-local.el --- Personal User settings -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; set font size
+(set-face-attribute 'default nil :height 150)
+
+(provide 'init-local)
