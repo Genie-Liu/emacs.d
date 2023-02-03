bac71e7ccff3f06d830e0fbf47eeeefba2ab6560
Jianshi Liu <jianshiliu@tubi.tv>
2023-02-03 13:16:47 +0800

    add cape

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -28,6 +28,23 @@
   ;; TODO: https://github.com/jdtsmith/kind-icon
   )
 
+;; Add extensions
+
+;; (when  (maybe-require-package 'cape)
+;;   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+;;   (add-to-list 'completion-at-point-functions #'cape-file)
+;;   )
+
+;;(add-to-list 'completion-at-point-functions #'cape-history)
+;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+;;(add-to-list 'completion-at-point-functions #'cape-tex)
+;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+;;(add-to-list 'completion-at-point-functions #'cape-ispell)
+;;(add-to-list 'completion-at-point-functions #'cape-dict)
+;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+;;(add-to-list 'completion-at-point-functions #'cape-line)
 
 (provide 'init-corfu)
 ;;; init-corfu.el ends here
